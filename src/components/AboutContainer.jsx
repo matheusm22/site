b018b79fbebd77d6d92b7f6b95554245a1e7dabf
Matheus Motta <matheus.mottademello@gmail.com>
2024-01-19 01:00:01 -0300

    V

--- src/components/AboutContainer.jsx
+++ src/components/AboutContainer.jsx
@@ -3,10 +3,12 @@ const AboutContainer = () => {
     <section className="about-container">
       <h2>Sobre</h2>
       <p>
-      Possuo uma grande vontade de aprender sempre algo sobre tecnologia, amo o que eu faço, e quero continuar com essa mesma paixão daqui 10 ou 20 anos, eu utilizo muito Mysql, 
-      php e JS acho uma otima combinação de db e software, 
+      Possuo uma grande vontade de sempre aprender algo sobre tecnologia, 
+      amo o que eu faço, e quero continuar com essa mesma paixão daqui 10 ou 20 anos, 
+      eu utilizo muito Mysql, php e JS acho uma otima combinação entre db e software, 
       tenho iniciativa estudo e leio bastante, principalmente artigos. 
-      A minha expectativa como desenvolvedor é me tornar tech-lead um dia, e liderar uma grande equipe de desenvolvimento.
+      A minha expectativa como desenvolvedor é me tornar tech-lead um dia 
+      e liderar uma grande equipe de desenvolvimento.
       </p>
     </section>
   );
