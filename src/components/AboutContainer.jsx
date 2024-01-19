6dd362fcbd16c2b6013fdad7c2f804c2c3b515bd
Matheus Motta <matheus.mottademello@gmail.com>
2024-01-19 01:08:53 -0300

    testes de deploy

--- src/components/AboutContainer.jsx
+++ src/components/AboutContainer.jsx
@@ -7,7 +7,7 @@ const AboutContainer = () => {
       amo o que eu faço, e quero continuar com essa mesma paixão daqui 10 ou 20 anos, 
       eu utilizo muito Mysql, php e JS acho uma otima combinação entre db e software, 
       tenho iniciativa estudo e leio bastante, principalmente artigos. 
-      A minha expectativa como desenvolvedor é me tornar tech-lead um dia 
+      A minha expectativa como desenvolvedor é me tornar tech-lead um d 
       e liderar uma grande equipe de desenvolvimento.
       </p>
     </section>
