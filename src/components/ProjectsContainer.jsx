1fe4a0b75e4f984d5694c03a0f72d206c17a3735
matheus_motta <43185269+matheusm22@users.noreply.github.com>
2025-07-24 13:36:51 -0300

    Update ProjectsContainer.jsx

--- src/components/ProjectsContainer.jsx
+++ src/components/ProjectsContainer.jsx
@@ -4,7 +4,7 @@ const ProjectsContainer = () => {
       <h2>Projetos</h2>
       <p>
         Alguns projetos como automação em python para o google contatos, sistema de solicitações para suporte técnico,
-         e até uma réplica do google meet, essa página foi desenvolvida com REACT.JS e SASS.
+        e até uma réplica do google meet, essa página foi desenvolvida com REACT.JS e SASS.
       </p>
       <a href="https://github.com/matheusm22" className="btn">
         Ver Projetos
