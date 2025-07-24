685f31548e6593b3771329d91382b7c1de5ff17f
matheus_motta <43185269+matheusm22@users.noreply.github.com>
2025-07-24 16:17:32 -0300

    Update SocialNetworkContainer.jsx

--- src/components/SocialNetworkContainer.jsx
+++ src/components/SocialNetworkContainer.jsx
@@ -1,4 +1,4 @@
-import { FaLinkedinIn, FaGithub, FaInstagram } from "react-icons/fa";
+import { FaLinkedinIn, FaGithub, FaInstagram, FaWheelchair } from "react-icons/fa";
 
 import "../styles/components/socialnetworkcontainer.sass";
 
@@ -6,6 +6,7 @@ const socialNetworks = [
   { name: "linkedin", icon: <FaLinkedinIn />, link: "https://www.linkedin.com/in/matheusm22"},
   { name: "github", icon: <FaGithub />, link: "https://github.com/matheusm22"},
   { name: "instagram", icon: <FaInstagram />, link: "https://www.instagram.com/matheusmm22/" },
+  { name: "instagram", icon: <FaWheelchair/>},
 ];
 
 const SocialNetworkContainer = () => {
